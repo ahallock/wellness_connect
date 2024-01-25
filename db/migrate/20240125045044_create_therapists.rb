6b044fe6bae075fc1d93b62a748ac5c1fe73bcf8
Andrew Hallock <andrew.hallock@gmail.com>
2024-01-24 23:57:06 -0500

    add therapists table migration

---
+++ db/migrate/20240125045044_create_therapists.rb
@@ -0,0 +1,18 @@
+class CreateTherapists < ActiveRecord::Migration[7.1]
+  def change
+    create_table :therapists do |t|
+      t.string :first_name, null: false, limit: 200
+      t.string :last_name, null: false, limit: 200
+      t.boolean :telehealth, null: false
+      t.string :profession, null: false, limit: 200
+      t.boolean :license_status, null: false
+      t.string :website, limit: 200
+      t.string :email, null: false, limit: 200
+      t.text :bio, null: false, limit: 2000
+      t.string :phone, null: false, limit: 200
+      t.string :avatar_url, null: false, limit: 200
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,29 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.1].define(version: 2024_01_25_045044) do
+  create_table "therapists", force: :cascade do |t|
+    t.string "first_name", limit: 200, null: false
+    t.string "last_name", limit: 200, null: false
+    t.boolean "telehealth", null: false
+    t.string "profession", limit: 200, null: false
+    t.boolean "license_status", null: false
+    t.string "website", limit: 200
+    t.string "email", limit: 200, null: false
+    t.text "bio", limit: 2000, null: false
+    t.string "phone", limit: 200, null: false
+    t.string "avatar_url", limit: 200, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end

4a0ab7d74d580f41c70755bdf07eb4055cc97c46
Andrew Hallock <andrew.hallock@gmail.com>
2024-01-25 00:30:50 -0500

    add offices table migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_25_045044) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_25_052812) do
+  create_table "offices", force: :cascade do |t|
+    t.string "name", limit: 200, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "therapists", force: :cascade do |t|
     t.string "first_name", limit: 200, null: false
     t.string "last_name", limit: 200, null: false

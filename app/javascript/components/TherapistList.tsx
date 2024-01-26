8cd7e45305a378a0189fd62a6c5822b6b0010254
Andrew Hallock <andrew.hallock@gmail.com>
2024-01-26 15:32:11 -0500

    add therapist slideover

--- app/javascript/components/TherapistList.tsx
+++ app/javascript/components/TherapistList.tsx
@@ -2,6 +2,7 @@ import * as React from "react"
 import { ChevronRight } from "lucide-react"
 import { Therapist } from "@/models";
 import SkeletonRow from "@/components/ui/skeleton_row"
+import TherapistSlideover from "@/components/TherapistSlideover"
 
 const TherapistList = ({ therapists, isLoading }: { therapists: Therapist[], isLoading: boolean }) => {
   return (
@@ -25,25 +26,7 @@ const TherapistList = ({ therapists, isLoading }: { therapists: Therapist[], isL
           className="divide-y divide-gray-100 overflow-hidden bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl"
         >
           {therapists.map((therapist) => (
-            <li key={`therapist-${therapist.id}`} className="relative flex justify-between gap-x-6 px-4 py-5 hover:bg-gray-50 sm:px-6">
-              <div className="flex min-w-0 gap-x-4">
-                <img className="h-12 w-12 flex-none rounded-full bg-gray-50" src={therapist.avatar_url} alt="" />
-                <div className="min-w-0 flex-auto">
-                  <p className="text-sm font-semibold leading-6 text-gray-900">
-                    <a href={therapist.website}>
-                      <span className="absolute inset-x-0 -top-px bottom-0" />
-                      {therapist.first_name} {therapist.last_name}
-                    </a>
-                  </p>
-                  <p className="mt-1 flex text-xs leading-5 text-gray-500">
-                    {therapist.profession}
-                  </p>
-                </div>
-              </div>
-              <div className="flex shrink-0 items-center gap-x-4">
-                <ChevronRight className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
-              </div>
-            </li>
+            <TherapistSlideover key={`therapist-${therapist.id}`} therapist={therapist} />
           ))}
         </ul>
        )}
